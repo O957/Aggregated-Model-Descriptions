93e89c2d8a1bd9cde3434c19d8d9a3a7df97af02
O957 <127630341+O957@users.noreply.github.com>
2025-12-02 14:40:29 -0500

    begin forecast fire example

---
+++ examples/forest_fires_Robert-TBCFRDTFTCI-ED2-2007/forest_fire_Robert-TBCFRDTFTCI-ED2-2007.py
@@ -0,0 +1,18 @@
+"""
+The forecast fie model specified on page 2 of The Bayesian
+Choice From Decision-Theoretic Foundations To Compuational
+Implementation, Edition 2, by Christian P. Robert
+(Robert-TBCFRDTFTCI-ED2-2007).
+"""
+
+
+def model():
+    pass
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
